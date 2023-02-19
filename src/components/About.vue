a1248d6924bb2563fc7795f5993ba1d392c62ee7
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-02-20 00:21:00 +0700

    About: Fix text-size in widescreen
    For screen with minimum width 1280px, use text-size with
    size is 1.25rem, and for screen with minimum width 1920px,
    use text-size with size is 1.5rem.
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,8 +10,8 @@
                     <v-col class="flex flex-initial justify-center md:3/6 xl:w-2/6">
                         <img src="@/assets/logo.webp" alt="" srcset="" class="max-h-60 md:max-h-80">
                     </v-col>
-                    <v-col class="text-lg lg:text-xl xl:text-2xl mt-5 xl:mt-0 leading-tight w-4/6">
-                        <h1 class="text-3xl lg:text-4xl xl:text-5xl pb-10">Tentang Komunitas Bocchi the Rock Indonesia</h1>
+                    <v-col class="text-lg lg:text-xl 3xl:text-2xl mt-5 xl:mt-0 leading-tight w-4/6">
+                        <h1 class="text-3xl lg:text-4xl 3xl:text-5xl pb-10">Tentang Komunitas Bocchi the Rock Indonesia</h1>
                         <div class="space-y-4">
                             <p>Komunitas Bocchi the Rock Indonesia atau disebut Bocchi the Rock Indonesia (BTRI) adalah sebuah komunitas penggemar serial Boochi the rock.</p>
                             <p>Bocchi the Rock Indonesia didirikan pada tanggal 7 November 2022 di Facebook dan 21 Desember 2022 di Discord.</p>
@@ -22,15 +22,15 @@
             </v-container>
             <v-container class="pb-5 xl:py-10">
                 <v-row>
-                    <v-col class="text-lg lg:text-xl xl:text-2xl leading-tight">
-                        <h1 class="text-3xl lg:text-4xl xl:text-5xl pb-10">Tentang Bocchi the Rock!</h1>
-                        <h3 class="text-xl lg:text-2xl xl:text-3xl pb-5">Penjelasan Singkat</h3>
+                    <v-col class="text-lg lg:text-xl 3xl:text-2xl leading-tight">
+                        <h1 class="text-3xl lg:text-4xl 3xl:text-5xl pb-10">Tentang Bocchi the Rock!</h1>
+                        <h3 class="text-xl lg:text-2xl 3xl:text-3xl pb-5">Penjelasan Singkat</h3>
                         <div class="space-y-4">
                             <p><span class="font-bold">Bocchi the Rock</span> (ぼっち・ざ・ろっく！ <span class="italic">Bocchi za Rokku!</span>) adalah manga <span class="italic">4-koma</span> atau manga 4 panel yang ditulis dan digambar oleh Aki Hamaji dan diserialisasikan di majalah <span class="italic">Manga Time Kirara Max</span> sejak Desember 2017.</p>
                             <p>Manga ini telah dianimasikan oleh studio CloverWorks dan ditayangkan dari tanggal 9 Oktober hingga 25 Desember 2022.</p>
                         </div>
 
-                        <h3 class="text-xl lg:text-2xl xl:text-3xl pt-12 pb-5">Sinopsis</h3>
+                        <h3 class="text-xl lg:text-2xl 3xl:text-3xl pt-12 pb-5">Sinopsis</h3>
                         <div class="space-y-4">
                             <p>Serial ini menceritakan karakter bernama Hitori "Bocchi" Gotou, seorang gadis SMP yang pemalu. Dia mulai belajar gitar seorang diri dan berkhayal tampil di panggung bersama grup bandnya.</p>
                             <p>Namun, harapan Bocchi tak berjalan sesuai mimpinya, sampai akhirnya Bocchi duduk di bangku SMA. Di masa itulah dia bertemu dengan seorang <span class="italic">drummer</span> bernama Nijika Ijichi. Nijika mengajaknya bergabung dengan Kessoku Band.</p>
@@ -38,7 +38,7 @@
                             <a href="https://duniaku.idntimes.com/anime-manga/anime-lain/seo-intern-duniaku/sinopsis-bocchi-the-rock">Sumber</a>
                         </div>
 
-                        <h3 class="text-xl lg:text-2xl xl:text-3xl pt-12 pb-5">Informasi Detail</h3>
+                        <h3 class="text-xl lg:text-2xl 3xl:text-3xl pt-12 pb-5">Informasi Detail</h3>
                         <v-table>
                             <tbody>
                                 <tr>
@@ -60,7 +60,7 @@
                             </tbody>
                         </v-table>
 
-                        <h3 class="text-xl lg:text-2xl xl:text-3xl pt-12 pb-5">Streaming Platform</h3>
+                        <h3 class="text-xl lg:text-2xl 3xl:text-3xl pt-12 pb-5">Streaming Platform</h3>
                         <v-row>
                             <v-col class="stream-link">
                                 <a href="https://www.bilibili.tv/en/media/2069564" class="text-3xl" title="Bstation"><i class="ri-bilibili-line"></i></a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1920px',
+      }
+    },
   },
   plugins: [
   ],
